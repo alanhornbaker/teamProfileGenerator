f62f7e3070b15366507b948d8e6d801178a62538
Alan Hornbaker <alanhornbaker@gmail.com>
2023-02-23 21:44:09 -0600

    desperation commit before submission

--- Develop/test/intern.test.js
+++ Develop/test/intern.test.js
@@ -23,6 +23,7 @@ describe("Intern", () => {
       });
     });
   });
+
   describe("method tests", () => {
     test("should return id when the getId method is called", () => {
       const intern = new Intern(mockIntern);
@@ -31,12 +32,12 @@ describe("Intern", () => {
 
     test("should return name when the getName method is called", () => {
       const intern = new Intern(mockIntern);
-      expect(intern.getName()).toEqual("Ella");
+      expect(intern.getName()).toEqual("Sam");
     });
 
     test("should return email when the getEmail method is called", () => {
       const intern = new Intern(mockIntern);
-      expect(intern.getEmail()).toEqual("ella@codeworks.com");
+      expect(intern.getEmail()).toEqual("sam@codeworks.com");
     });
 
     test("should return school when the getSchool method is called", () => {
@@ -46,7 +47,7 @@ describe("Intern", () => {
 
     test("should return intern role when the getRole method is called", () => {
       const intern = new Intern(mockIntern);
-      expect(intern.getEmail()).toEqual("intern");
+      expect(intern.getRole()).toEqual("Intern");
     });
   });
 });
